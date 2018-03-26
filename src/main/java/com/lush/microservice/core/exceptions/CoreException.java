2a82b0f21f88cf9811564bb269a77b26220f555d
is <is@lush.co.kr>
2018-03-26 09:46:50 +0900

    Add ExceptionType and CoreException java files

---
+++ src/main/java/com/lush/microservice/core/exceptions/CoreException.java
@@ -0,0 +1,69 @@
+package com.lush.microservice.core.exceptions;
+
+import com.lush.microservice.core.enums.ResponseStatusType;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+/**
+ * Core Exception(Common)
+ *
+ * @author Jelly
+ * @author Is
+ */
+@Component
+public class CoreException extends RuntimeException {
+
+  /**
+   * Create default serialVersionUID.
+   */
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * Exceptoin status
+   */
+  private final String status;
+
+  /**
+   * Exceptoin code
+   */
+  private final Integer code;
+
+  /**
+   * Exceptoin message
+   */
+  private final String message;
+
+  /**
+   * The default creator. (using default code and message)
+   */
+  public CoreException() {
+    this.status = ResponseStatusType.FAIL.getStatus();
+    this.code = HttpStatus.INTERNAL_SERVER_ERROR.value();
+    this.message = "Internal server exception";
+  }
+
+  /**
+   * The default creator. (User Created)
+   *
+   * @param code
+   * @param handlerMessage
+   */
+  public CoreException(Integer code, String handlerMessage) {
+    this.status = ResponseStatusType.FAIL.getStatus();
+    this.code = HttpStatus.INTERNAL_SERVER_ERROR.value();
+    this.message = handlerMessage;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public Integer getCode() {
+    return code;
+  }
+
+  @Override
+  public String getMessage() {
+    return message;
+  }
+}
