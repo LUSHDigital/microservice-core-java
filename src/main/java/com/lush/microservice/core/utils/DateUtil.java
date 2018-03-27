1a2f595180fad87ac3ed9abf993fd3fa5d3f5575
gameover3 <gameover8411@gmail.com>
2018-03-27 14:37:43 +0900

    Update DateUtil.java

--- src/main/java/com/lush/microservice/core/utils/DateUtil.java
+++ src/main/java/com/lush/microservice/core/utils/DateUtil.java
@@ -15,31 +15,41 @@ import org.springframework.stereotype.Component;
 @Component
 public class DateUtil {
 
-  // get now local year
+  /**
+   * Get Now local year(only year)
+   */
   public int getNowYear() {
     int nowYear = LocalDate.now().getYear();
     return nowYear;
   }
 
-  // get now local month
+  /**
+   * Get Now local month(only month)
+   */
   public int getNowMonth() {
     int nowMonth = LocalDate.now().getMonthValue();
     return nowMonth;
   }
 
-  // get now local date
+  /**
+   * Get Now local date(only date)
+   */
   public LocalDate getNowDate() {
     LocalDate nowDate = LocalDate.now();
     return nowDate;
   }
 
-  // get now local time
+  /**
+   * Get Now local time(year, monthm, date, time)
+   */
   public LocalDateTime getNowTime() {
     LocalDateTime nowTime = LocalDateTime.now();
     return nowTime;
   }
 
-  // get now time only
+  /**
+   * Get Now local time only(only time)
+   */
   public LocalTime getOnlyNowTime() {
     LocalTime nowOnlyNowTime = LocalTime.now();
     return nowOnlyNowTime;
